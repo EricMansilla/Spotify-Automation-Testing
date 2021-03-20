61094af211a534954ff2e369dfaae1bf7e1a30b9
EricMansilla <eric.324.st@gmail.com>
2021-03-20 19:30:53 -0300

    First UI Interactions - Click and SendKeys

--- src/test/java/StepDefinitions/StepDefinitions.java
+++ src/test/java/StepDefinitions/StepDefinitions.java
@@ -1,8 +1,12 @@
 package StepDefinitions;
 
 import Functions.CreateDriver;
+import Functions.SeleniumFunctions;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
@@ -30,4 +34,36 @@ public class StepDefinitions {
         driver.get(url);
     }
 
+    @Given("^I navigate to (.*)$")
+    public void navigateTo(String url) {
+        log.info("Navigate to: " + url);
+        driver.get(url);
+    }
+
+    @Then("^I load the DOM Information (.*)$")
+    public void iLoadTheDOMInformation(String file) throws Exception {
+        SeleniumFunctions.FileName = file;
+        SeleniumFunctions.readJson();
+        log.info("initialize file: " + file );
+    }
+
+    @And("I click on element (.*)")
+    public void iClickOnElement(String locator) throws Exception {
+        By SeleniumElem = SeleniumFunctions.getCompleteElement(locator);
+        driver.findElement(SeleniumElem).click();
+        log.info("Element " + locator + " clicked.");
+    }
+
+    @And("^I set (.*) with text (.*)$")
+    public void iSetWithText(String element, String text) throws Exception {
+        By SeleniumElem = SeleniumFunctions.getCompleteElement(element);
+        driver.findElement(SeleniumElem).sendKeys(text);
+        log.info("Sended " + text + "to element " + element);
+    }
+
+
+    @Then("I close the window")
+    public void iCloseTheWindow() {
+        driver.quit();
+    }
 }
--- src/test/resources/Features/BaseTest.feature
+++ src/test/resources/Features/BaseTest.feature
@@ -3,3 +3,11 @@ Feature: SpotifyRegisterTest
   @test
   Scenario: Get Sites
     Given I am in App main site
+  
+  @test
+  Scenario: I click an object
+    Given I navigate to https://www.spotify.com/py/signup/
+    Then I load the DOM Information Spotify_registro.json
+    And I click on element Email
+    And I set Email with text prueba@prueba.com
+    Then I close the window
